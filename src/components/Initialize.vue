6ce0b784eed136089c4f5002fa23eed7a4cf6cb5
jonathanhumphrey <jonny105596@outlook.com>
2022-06-13 11:45:51 -0500

    prod URL added

--- src/components/Initialize.vue
+++ src/components/Initialize.vue
@@ -3,7 +3,7 @@
     <a
       class="link-account-anchor"
       id="linkAnchor"
-      href="https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=pk0roinew9e83z6qn6ctr7xo7yas15&redirect_uri=http://localhost:8080&scope=user:read:follows%20channel:read:subscriptions%20user:read:follows"
+      href="https://id.twitch.tv/oauth2/authorize?response_type=token&client_id=pk0roinew9e83z6qn6ctr7xo7yas15&redirect_uri=https://62a767df1e68ea0bab77f158--lovely-tiramisu-43f35a.netlify.app/&scope=user:read:follows%20channel:read:subscriptions%20user:read:follows"
       >Authorize a Link</a
     >
     <button class="link-button" @click="validateToken()">Link Now!</button>
@@ -12,6 +12,9 @@
 
 <script>
 import { mapActions, mapState } from "vuex";
+// FOR LOCAL HOST: http://localhost:8080
+// FOR NETLIFY: https://62a767df1e68ea0bab77f158--lovely-tiramisu-43f35a.netlify.app/
+
 /* 
 http://localhost:8080/#access_token=if94bmlel947n7g4efdzkfhzvkxe8p&scope=user%3Aread%3Afollows&token_type=bearer
 */
